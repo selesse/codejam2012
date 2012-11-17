935168037961938ad685ae7e1364decbcb2c6009
Alex Bourgeois <abourg28@gmail.com>
2012-11-16 23:36:14 -0500

    Formatted. Added skeleton for EMAStrategy.

---
+++ code/CodeJam2012/src/com/rathesh/codejam2012/server/strategies/EMAStrategy.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.rathesh.codejam2012.server.strategies;
+
+/**
+ * @author Alex Bourgeois
+ *
+ */
+public class EMAStrategy extends AbstractStrategy {
+
+  public EMAStrategy(int N) {
+    super(N);
+  }
+
+}
--- code/CodeJam2012/src/com/rathesh/codejam2012/server/strategies/SMAStrategy.java
+++ code/CodeJam2012/src/com/rathesh/codejam2012/server/strategies/SMAStrategy.java
@@ -5,31 +5,32 @@ package com.rathesh.codejam2012.server.strategies;
 
 /**
  * @author Alex Bourgeois
- *
+ * 
  */
 public class SMAStrategy extends AbstractStrategy {
 
   public SMAStrategy(int N) {
     super(N);
   }
-  
+
   @Override
   public double update(double price) {
     // Update prices queue
     double ptMinusN = this.prices.poll();
     this.prices.add(price);
-    
+
     // Update averages list
     double SMAt = 0;
     int t = this.prices.size();
     if (t > N) {
-      SMAt = getAverage() - (ptMinusN/N) + (price/N);
-    } else {
+      SMAt = getAverage() - (ptMinusN / N) + (price / N);
+    }
+    else {
       for (double p : this.prices) {
-        SMAt += p/t;
+        SMAt += p / t;
       }
     }
-    this.averages.add(SMAt);    
+    this.averages.add(SMAt);
     return SMAt;
   }
 }
