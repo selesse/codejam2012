3bf570a10298be26009c055b10779a2f978791bd
Alex Selesse <alex@selesse.com>
2012-11-16 21:33:09 -0500

    Code format - initial commit.

--- code/CodeJam2012/src/com/rathesh/codejam2012/server/GreetingServiceImpl.java
+++ code/CodeJam2012/src/com/rathesh/codejam2012/server/GreetingServiceImpl.java
@@ -11,7 +11,7 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 public class GreetingServiceImpl extends RemoteServiceServlet implements GreetingService {
 
   public String greetServer(String input) throws IllegalArgumentException {
-    // Verify that the input is valid. 
+    // Verify that the input is valid.
     if (!FieldVerifier.isValidName(input)) {
       // If the input is not valid, throw an IllegalArgumentException back to
       // the client.
@@ -25,14 +25,16 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements Greetin
     input = escapeHtml(input);
     userAgent = escapeHtml(userAgent);
 
-    return "Hello, " + input + "!<br><br>I am running " + serverInfo + ".<br><br>It looks like you are using:<br>" + userAgent;
+    return "Hello, " + input + "!<br><br>I am running " + serverInfo
+        + ".<br><br>It looks like you are using:<br>" + userAgent;
   }
 
   /**
    * Escape an html string. Escaping data received from the client helps to
    * prevent cross-site script vulnerabilities.
    * 
-   * @param html the html string to escape
+   * @param html
+   *          the html string to escape
    * @return the escaped string
    */
   private String escapeHtml(String html) {
