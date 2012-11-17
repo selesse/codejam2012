029b0f38569da5ae670eab7f5c7f72a1491eb0cf
Rathesh Sabapathipilli <rathesh.saba@gmail.com>
2012-11-16 23:08:31 -0500

    AbstractStrategy Updated and LWMA alg. uploaded

--- code/CodeJam2012/src/com/rathesh/codejam2012/server/strategies/AbstractStrategy.java
+++ code/CodeJam2012/src/com/rathesh/codejam2012/server/strategies/AbstractStrategy.java
@@ -4,20 +4,18 @@ import java.util.List;
 import java.util.Queue;
 import com.google.common.collect.*;
 
-
-
 public abstract class AbstractStrategy implements Strategy {
 
   int N;
   protected Queue<Double> prices;
   protected List<Double> averages;
-  
+
   public AbstractStrategy(int N) {
     this.N = N;
     prices = Queues.newArrayBlockingQueue(N);
     averages = Lists.newArrayList();
   }
-  
+
   @Override
   public double update(double price) {
     // TODO Auto-generated method stub
@@ -26,6 +24,9 @@ public abstract class AbstractStrategy implements Strategy {
 
   @Override
   public double getAverage() {
+    if (averages.size() - 1 < 0) {
+      return -1;
+    }
     return this.averages.get(averages.size() - 1);
   }
 
