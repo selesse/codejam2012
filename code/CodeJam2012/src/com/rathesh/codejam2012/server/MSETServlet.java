3432609a73d2dcd582ca9db58373f957addb583e
Alex Bourgeois <abourg28@gmail.com>
2012-11-17 14:13:21 -0500

    Created skeleton for servlet.

---
+++ code/CodeJam2012/src/com/rathesh/codejam2012/server/MSETServlet.java
@@ -0,0 +1,62 @@
+package com.rathesh.codejam2012.server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class MSETServlet extends HttpServlet {
+  
+  public static final String DOCTYPE =
+      "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">";
+
+  public static String headWithTitle(String title) {
+    return(DOCTYPE + "\n" +
+           "<HTML>\n" +
+           "<HEAD><TITLE>" + title + "</TITLE></HEAD>\n");
+  }
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+      IOException {
+    
+    
+    
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    out.println(headWithTitle("Hello WWW") +
+                "<BODY>\n" +
+                "<H1>Hello WWW</H1>\n" +
+                "</BODY></HTML>");
+  }
+  
+  protected static void startStockExchange() {
+    // TODO in here we need to
+    // 0. Create strategies and managers
+    // 1. Get the price feed (send 'H' to msExchange)
+    // 2. Set the trade booking socket (global)
+    // 3. While still receiving prices (not receiving 'C')
+    // 4. Update strategies which will update managers, Managers will call sendBuy or Sell
+    // 5. Update clock
+  }
+  
+  public static void sendSell(String name, String type) {
+    // TODO in here we need to
+    // Send 'S' through trade booking socket
+    // The exchange responds with a price, keep note of it for silanis :)
+  }
+  
+  public static void sendBuy(String name, String type) {
+    // TODO in here we need to
+    // Send 'B' through trade booking socket
+    // The exchange responds with a price, keep note of it for silanis :)
+    
+  }
+  
+}
