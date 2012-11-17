a21c41248e863905b1722d0673fb7f1421003c0e
Alex Selesse <alex@selesse.com>
2012-11-17 13:47:13 -0500

    Changes to main.js.

--- code/CodeJam2012/war/js/main.js
+++ code/CodeJam2012/war/js/main.js
@@ -2,8 +2,11 @@ $(document).ready(function() {
   show_manager_schedule();
 
   $("button#go").click(function() {
-    $("div#schedule").toggleClass("hidden");
-    $("div#graphs").toggleClass("hidden");
+    // swap the hidden status of graphs & schedule... only at the beginning
+    if ($("div#graphs").hasClass("hidden")) {
+      $("div#schedule").toggleClass("hidden");
+      $("div#graphs").toggleClass("hidden");
+    }
     plot_graph("div#sma-graph");
     plot_graph("div#lwma-graph");
     plot_graph("div#ema-graph");
@@ -11,28 +14,41 @@ $(document).ready(function() {
   });
 });
 
-function plot_graph(id) {
+function plot_graph(id, data) {
+  data = data || [[0, 0], [1, 1]];
   // clear the HTML otherwise continually calling $.plot extends height
   $(id).html("");
   $.plot( $(id),
-    [ { label : "Price", data : [[0, 0], [1, 1]] } ],
+    [ { label : "Price", data : data } ],
     { yaxis: { max: 1 } }
   );
 }
 
-function show_manager_schedule(num_managers) {
+function show_manager_schedule(num_managers, schedule) {
   var id = "div#schedule";
-  var schedule = [
-    [Date.UTC(2012, 0, 1, 11), 1, Date.UTC(2012, 0, 1, 13), "Working..."],
-    [Date.UTC(2012, 0, 1, 13, 30), 1, Date.UTC(2012, 0, 1, 15, 30), "Working..."],
-    [Date.UTC(2012, 0, 1, 9), 2, Date.UTC(2012, 0, 1, 14), "Working..."],
+  var num_managers = num_managers || 4;
+
+  var schedule = schedule || [
+    [Date.UTC(2012, 0, 1, 9), 4, Date.UTC(2012, 0, 1, 11), "Working"],
+    [Date.UTC(2012, 0, 1, 11, 30), 4, Date.UTC(2012, 0, 1, 13, 30), "Working"],
+
+    [Date.UTC(2012, 0, 1, 10), 3, Date.UTC(2012, 0, 1, 12), "Working"],
+    [Date.UTC(2012, 0, 1, 12, 30), 3, Date.UTC(2012, 0, 1, 14, 30), "Working"],
+
+    [Date.UTC(2012, 0, 1, 12), 2, Date.UTC(2012, 0, 1, 14), "Working"],
+    [Date.UTC(2012, 0, 1, 14, 30), 2, Date.UTC(2012, 0, 1, 16, 30), "Working"],
+
+    [Date.UTC(2012, 0, 1, 13, 30), 1, Date.UTC(2012, 0, 1, 15, 30), "Working"],
+    [Date.UTC(2012, 0, 1, 16), 1, Date.UTC(2012, 0, 1, 18), "Working"],
   ];
 
   var breaks = [
-    [Date.UTC(2012, 0, 1, 13), 1, Date.UTC(2012, 0, 1, 13, 30), "Break"],
+    [Date.UTC(2012, 0, 1, 11), 4, Date.UTC(2012, 0, 1, 11, 30), "Break"],
+    [Date.UTC(2012, 0, 1, 12), 3, Date.UTC(2012, 0, 1, 12, 30), "Break"],
+    [Date.UTC(2012, 0, 1, 14), 2, Date.UTC(2012, 0, 1, 14, 30), "Break"],
+    [Date.UTC(2012, 0, 1, 15, 30), 1, Date.UTC(2012, 0, 1, 16), "Break"],
   ];
 
-  var num_managers = num_managers || 3;
   var managers = [];
   var ticks = [];
 
