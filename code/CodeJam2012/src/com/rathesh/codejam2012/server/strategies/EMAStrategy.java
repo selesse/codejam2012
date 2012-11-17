7903f9cbf6ad00e6520f0c32577d99493e1d663c
Rathesh Sabapathipilli <rathesh.saba@gmail.com>
2012-11-17 09:47:29 -0500

    EMAStrategy implemented

--- code/CodeJam2012/src/com/rathesh/codejam2012/server/strategies/EMAStrategy.java
+++ code/CodeJam2012/src/com/rathesh/codejam2012/server/strategies/EMAStrategy.java
@@ -5,12 +5,30 @@ package com.rathesh.codejam2012.server.strategies;
 
 /**
  * @author Alex Bourgeois
- *
+ * 
  */
 public class EMAStrategy extends AbstractStrategy {
+  double alpha;
 
   public EMAStrategy(int N) {
     super(N);
+    alpha = (2 / ((double) N + 1));
+  }
+
+  @Override
+  public double update(double price) {
+    double average;
+    if (this.getAverage() == -1) {
+      average = price;
+      this.averages.add(price);
+    }
+    else {
+      double prevAvg = this.getAverage();
+      average = prevAvg + alpha * (price - prevAvg);
+      this.averages.add(average);
+    }
+
+    return average;
   }
 
 }
